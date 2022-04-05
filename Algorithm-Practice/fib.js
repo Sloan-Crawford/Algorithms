45c1ab567e55175caf05065d5a87ad2358e03c72
Sloan-Crawford <sloan.crawford@gmail.com>
2022-04-05 08:46:25 -0400

    Added Fibonacci Memoization

--- Algorithm-Practice/fib.js
+++ Algorithm-Practice/fib.js
@@ -21,10 +21,10 @@ const fib = (n) => {
  return fib(n-1) + fib(n-2);
 };
 
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
-console.log(fib(50)); // this one takes a really long time (110sec), so too slow
+console.log(fib(6)); // 8
+console.log(fib(7)); // 13
+console.log(fib(8)); // 21
+console.log(fib(50)); // 12586269025... this one takes a really long time (110sec), so too slow
 
 // it has correctness so far, but lacks efficiency...
 
@@ -36,3 +36,34 @@ console.log(fib(50)); // this one takes a really long time (110sec), so too slow
 // meaning fib(50) = 2^50 steps, which is over 1 quadrillion steps
 
 // things to consider: Base Case and Recursive Case
+
+// the bottleneck comes from the time complexity, which stems from the number of recursive calls made
+// so I need to look for any patterns in the recursive nature of the problem
+// patterns of overlapping subproblems is known as Dynamic Programming. This can be applied when we have some larger problem (i.e., Fibonacci) and we can decompose it into smaller instances of the same problem, with an overlapping structure:
+
+// 1. calculate duplicate sub problems
+// 2. store those to be used later
+// 3. do this through memoization (memos or notes)
+// In JS use a JS object as a fast-access data structure (like a hashmap equivalent):
+// The keys will be arguments to the function, the values will be the return values
+// the memo is going to store n as the key and the values are the return values for the function.
+// first, check for existence inside our memo with if statement (add an additional base case)
+// if it does, return the value that corresponds to that memo's key
+// store the entire result inside my memo (the key is always what was used as the argument)
+// make sure all the recursive function calls are accesing the same memo so pass in that object as additional arguments to both of the calls
+// in sum, I added a new argument, a new base case, and memo storing logic
+
+const fib = (n, memo = {}) => {
+ if (n in memo) return memo[n];
+ if (n <= 2) return 1;
+ 
+ memo[n] = fib(n-1, memo) + fib(n-2, memo);
+ return memo[n];
+}
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
+
+// [Done] exited with code=0 in 0.281 seconds!
